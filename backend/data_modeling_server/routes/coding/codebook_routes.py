5b8e59418034fb96ffcf10b265b643056cea12af
ansh <daemononcode@gmail.com>
2025-05-20 07:03:20 -0400

    Update torrent

--- backend/data_modeling_server/routes/coding/codebook_routes.py
+++ backend/data_modeling_server/routes/coding/codebook_routes.py
@@ -72,7 +72,7 @@ async def generate_codebook_without_quotes_endpoint(
         store_response = False
     )
     
-    summarized_grouped_ec = {code: summary for code, summary in summarized_dict.items()}
+    summarized_grouped_ec = {code: [summary] for code, summary in summarized_dict.items()}
 
     print(summarized_grouped_ec)
 
@@ -80,6 +80,19 @@ async def generate_codebook_without_quotes_endpoint(
         initial_codebook_repo.delete({"coding_context_id": workspace_id})
     except Exception as e:
         print(e)
+
+    parsed_response = await process_llm_task(
+        workspace_id=request.headers.get("x-workspace-id"),
+        app_id=app_id,
+        manager=manager,
+        llm_model=request_body.model,
+        parent_function_name=function_name,
+        regex_pattern=r"```json\s*([\s\S]*?)\s*```",
+        prompt_builder_func=GenerateCodebookWithoutQuotes.generate_codebook_without_quotes_prompt,
+        llm_instance=llm,
+        llm_queue_manager=llm_queue_manager,
+        codes=json.dumps(summarized_grouped_ec)  
+    )
     
     initial_codebook_repo.insert_batch(
         [
@@ -88,7 +101,7 @@ async def generate_codebook_without_quotes_endpoint(
                 coding_context_id=request.headers.get("x-workspace-id"),
                 code= pr[0],
                 definition= pr[1]
-            ) for pr in  summarized_grouped_ec.items() 
+            ) for pr in  parsed_response.items() 
         ]
     )
 
